be29a9f02a21eb5f1aadf0312d7efc8d8d125860
littleattok <difj1324@gmail.com>
2024-11-08 01:24:02 +0900

    인터페이스 수정

--- src/main/java/com/example/attok/myguestbook/repository/GuestbookRepository.java
+++ src/main/java/com/example/attok/myguestbook/repository/GuestbookRepository.java
@@ -1,4 +1,14 @@
 package com.example.attok.myguestbook.repository;
 
-public interface GuestbookRepository {
+import com.example.attok.myguestbook.domain.Guestbook;
+import org.springframework.data.jpa.repository.JpaRepository;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.EntityManager;
+
+import java.util.List;
+
+public interface GuestbookRepository extends JpaRepository<Guestbook,Long>,GuestbookRepositoryCustom  {
+
+
+
 }
