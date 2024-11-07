3378edf199691b068e06e0c4cd38c26ae38494e5
littleattok <difj1324@gmail.com>
2024-11-08 01:20:55 +0900

    컨트롤러 초기 설정

--- src/main/java/com/example/attok/myguestbook/controller/GuestbookController.java
+++ src/main/java/com/example/attok/myguestbook/controller/GuestbookController.java
@@ -1,4 +1,28 @@
 package com.example.attok.myguestbook.controller;
 
+import com.example.attok.myguestbook.service.GuestbookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class GuestbookController {
+    @Autowired
+    private GuestbookService guestbookService;
+
+    @GetMapping("/guestbook")
+    public String guestbook() {
+        return "guestbook";
+    }
+
+    @PostMapping("/guestbook")
+    public String submitGuestbook() {
+        return "redirect:/guestbook";
+    }
+
+    @GetMapping("/guestbook/list")
+    public String listGuestbook() {
+        return "guestbook/list";
+    }
 }
