048105866538804f54ebe6dcdd95775be6941226
littleattok <difj1324@gmail.com>
2024-11-08 01:24:18 +0900

    상속 관련 수정

--- src/main/java/com/example/attok/myguestbook/repository/GuestbookRepositoryImpl.java
+++ src/main/java/com/example/attok/myguestbook/repository/GuestbookRepositoryImpl.java
@@ -1,5 +1,19 @@
 package com.example.attok.myguestbook.repository;
+import com.example.attok.myguestbook.domain.Guestbook;
 import com.example.attok.myguestbook.repository.GuestbookRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 
-public class GuestbookRepositoryImpl implements GuestbookRepository {
+import java.util.List;
+
+public class GuestbookRepositoryImpl implements GuestbookRepositoryCustom {
+    @PersistenceContext
+    private EntityManager entitymanager;
+
+    @Override
+    public List<Guestbook> findByNickname(String nickname) {
+        String jpql ="select g from Guestbook g where g.nickname= :nickname";
+        return entitymanager.createQuery(jpql, Guestbook.class)
+                .setParameter("nickname",nickname).getResultList();
+    }
 }
