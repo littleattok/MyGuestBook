0d7fe7e26b494ba9ad6dea21a2fe519a8120ef4b
littleattok <difj1324@gmail.com>
2024-11-08 01:21:09 +0900

    DTO 초기 설정

--- src/main/java/com/example/attok/myguestbook/domain/GuestbookDTO.java
+++ src/main/java/com/example/attok/myguestbook/domain/GuestbookDTO.java
@@ -1,4 +1,14 @@
 package com.example.attok.myguestbook.domain;
 
+
+import lombok.Data;
+
+@Data
 public class GuestbookDTO {
+    private Long id;
+    private String nickname;
+    private String password;
+    private String ip;
+    private String content;
+    private String regDate;
 }
