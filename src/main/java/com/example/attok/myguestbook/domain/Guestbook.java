a792d8bf5394d9d4ec89daf0890e5cea99406e4e
littleattok <difj1324@gmail.com>
2024-11-08 00:46:46 +0900

    기본키

--- src/main/java/com/example/attok/myguestbook/domain/Guestbook.java
+++ src/main/java/com/example/attok/myguestbook/domain/Guestbook.java
@@ -1,9 +1,18 @@
 package com.example.attok.myguestbook.domain;
 
 
+import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@Entity
+@Table(name="guestbook")
 public class Guestbook {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
 }
